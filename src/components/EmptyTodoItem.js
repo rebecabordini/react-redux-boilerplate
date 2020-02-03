71cb7e2a3e6f0123c57fe20e4199bfb428b1fc6c
Rebeca Bordini <rebeca.bordini@klarna.com>
2020-02-03 15:06:15 +0100

    format

--- src/components/EmptyTodoItem.js
+++ src/components/EmptyTodoItem.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+
+const enterPressed = event => {
+  if (event.keyCode === 13 || event.which === 13)
+    document.getElementById("addTodoButton").click();
+};
+
 
 export const EmptyTodoItem = ({ onChange }) => (
   <div>
-    <input type="text" id="newTodo" />
-    <button 
-      onClick={(e) => {
-        const value = document.getElementById('newTodo').value
-        onChange(value)
-    }}>Add</button>
+    <input id="newTodo" type="text" onKeyPress={enterPressed} />
+    <button
+      id="addTodoButton"
+      onClick={e => {
+        const value = document.getElementById("newTodo").value;
+        onChange(value);
+      }}
+    >
+      Add
+    </button>
   </div>
 )
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { updateTodos } from '../actions';
-import { EmptyTodoItem } from './EmptyTodoItem'
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const TodoList = ({todos, onChange}) => {
-  return (
-    <div>
-      <ul>
-        {todos.map(item => (
-          <li>{item}</li>
-        ))}
-      </ul>
-      <EmptyTodoItem onChange={onChange} />
-    </div>
-  )
-}
+import { addTodo } from "../actions";
+import { EmptyTodoItem } from "./EmptyTodoItem";
+import { TodoItem } from "./TodoItem";
 
 const mapStateToProps = state => {
   return {
-    todos: state.todos,
-  }
-}
+    todos: state.todos
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
-  onChange: (item) => { 
-    dispatch(updateTodos(item)) }
-})
+  onChange: item => {
+    dispatch(addTodo(item));
+  }
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
+const propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+const TodoList = ({ todos, onChange }) => {
+  const listItems = todos.map((item, index) => (
+    <TodoItem key={index} item={item} />
+  ));
+  return (
+    <div>
+      <ul>{listItems}</ul>
+      <EmptyTodoItem onChange={onChange} />
+    </div>
+  );
+};
+
+TodoList.propTypes = propTypes
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
